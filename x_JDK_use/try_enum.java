1c2708568dab2c99ac29d624a7d89a5b8def0555
wanghanqi <wanghanqi@baidu.com>
2017-06-16 21:04:33 +0800

    bye baidu

--- x_JDK_use/try_enum.java
+++ x_JDK_use/try_enum.java
@@ -7,7 +7,7 @@ enum Singleton{
 
     Singleton(){
     	System.out.println(System.currentTimeMillis());
-    	value = (int)System.currentTimeMillis();
+    	// value = (int)System.currentTimeMillis();
     }
     public int getValue() {
         return value;
@@ -24,20 +24,27 @@ enum Singleton{
 // }
 class try_enum{
 	public static void main(String[] args) throws Exception{
-		Singleton.A.setValue(123);
+		// Singleton.A.setValue(123);
+		System.out.println("______________________________");
 		System.out.println(Singleton.A.getValue());
-		try{
-			Thread.sleep(1);
-		}catch(Exception e){
+		// try{
+		// 	Thread.sleep(1);
+		// }catch(Exception e){
 
-		}
-		Singleton.B.setValue(321);
+		// }
+
+		// Singleton.B.setValue(321);
+
 		System.out.println(Singleton.B.getValue());
-
 		System.out.println(Singleton.C.getValue());
 
-		
-		
+		System.out.println("______________________________");
+		Singleton.A.setValue(123);
+
+		System.out.println(Singleton.A.getValue());
+		System.out.println(Singleton.B.getValue());
+		System.out.println(Singleton.C.getValue());
+
 		// System.out.println(Singleton.value);
 		// D d = new D();
 		// d.a = 13;
